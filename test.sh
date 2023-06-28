0776b137e2ab4672a4237a17c27bba6aec82cb32
Lorenzo Inigo C. Dela Cruz <lrnzdc@pop-os.localdomain>
2023-06-28 17:46:46 +0800

    testing

--- test.sh
+++ test.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-sudo su
-
 function custom_install {
   local package=$1
   echo "Do you want to install $package?"
   read -p "(y/N) " response
   if [[ $response =~ ^y ]]; then
-    sudo pacman -S --noconfirm $package &&
-      echo "$1 Installed" >> $LOG_FILE
+    sudo apt install -y $package &&
+      echo "$1 Installed"
   else
     echo "$1 NOT INSTALLED."
   fi
